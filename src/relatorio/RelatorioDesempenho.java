27c47a387c4fb56e26a7756d91d26ad1aaa9b384
itallo gravina <itallo.gravina@gmail.com>
2019-06-30 10:31:10 -0300

    modifocacoes nos relatorios

--- src/relatorio/RelatorioDesempenho.java
+++ src/relatorio/RelatorioDesempenho.java
@@ -1,5 +1,57 @@
 package relatorio;
+import registros.RegistrarAbastecimento;
+
 
 public class RelatorioDesempenho {
+   //private Desempenho.Abastecimento
+    private double KmLitro;
+    private double CustoKm;
+    private int KmAtual;
+    private int KmAnterior;
+    private double valor;
+    
+    public void MediaConsumo(){
+        
+    }
 
+    public double getKmLitro() {
+        return KmLitro;
+    }
+
+    public void setKmLitro(double KmLitro) {
+        this.KmLitro = KmLitro;
+    }
+
+    public double getCustoKm() {
+        return CustoKm;
+    }
+
+    public void setCustoKm(double CustoKm) {
+        this.CustoKm = CustoKm;
+    }
+
+    public int getKmAtual() {
+        return KmAtual;
+    }
+
+    public void setKmAtual(int KmAtual) {
+        this.KmAtual = KmAtual;
+    }
+
+    public int getKmAnterior() {
+        return KmAnterior;
+    }
+
+    public void setKmAnterior(int KmAnterior) {
+        this.KmAnterior = KmAnterior;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
 }
--- src/relatorio/RelatorioSimples.java
+++ src/relatorio/RelatorioSimples.java
@@ -1,4 +1,7 @@
 package relatorio;
+import registros.RegistrarAbastecimento;
+import registros.RegistrarTaxa;
+import registros.RegistrarManutencao;
 
 public class RelatorioSimples {
 
