9fb716cd2ebc6ee6b68c8c208ae8b806a5b600bd
Bruna <brunaalmeida48@gmail.com>
2019-07-08 13:03:45 -0300

    Mudanças nas classes de exceção

--- src/excecoes/CombustivelInvalidoException.java
+++ src/excecoes/CombustivelInvalidoException.java
@@ -1,19 +1,15 @@
 package excecoes;
 
 public class CombustivelInvalidoException extends Exception {
-private String mensagem;
 	
 	public CombustivelInvalidoException() {
-	
 	}
 
 	public CombustivelInvalidoException(String mensagem) {
-	super(mensagem);
-	this.mensagem = mensagem;
+		super(mensagem);
 	}
 	
 	public String toString() {
-	return "Tipo de combustível inválido! Informe um combustível válido.";
+		return "Tipo de combustível inválido! Informe um combustível válido.";
 	} 
-
 }
