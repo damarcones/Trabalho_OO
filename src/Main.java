1ce1874e75f647dc12bab231ea25418bca364ec0
itallogravina <51856522+itallogravina@users.noreply.github.com>
2019-07-12 10:28:48 -0300

    Update Main.java
    busca do veiculo por placa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import excecoes.*;
 import registros.RegistrarManutencao;
+import relatorio.RelatorioConsumoDoVeiculo;
 import relatorio.RelatorioSimples;
 import veiculo.Veiculo;
 import javax.swing.*;
@@ -19,9 +20,30 @@ public class Main {
 			+ "4) Relatorio de desempenho: Consumo;\n" + "5) Relatorio de desempenho: Custo por Km;\n" + "0) Voltar.\n"
 			+ "Escolha uma opcao:\n";
 
+	static ArrayList<Veiculo> veiculos = new ArrayList<>(); // criando uma arraylist do tipo veiculo
+	
+/////////////////////////////////METODO PARA ACHAR O ENDERECO DO VEICULO A PARTIR DA PLACA//////////////////////////////
+	public static int EnderecoDoCarro() {
+		int endereco_do_carro = -1;
+		try {
+			String placabuscada = JOptionPane.showInputDialog("Digite a Placa do Veiculo");
+			for(int i = 0; i < veiculos.size(); i++) {
+				if(veiculos.get(i).getPlaca().equals(placabuscada)) {
+					endereco_do_carro = i;
+				}
+			}
+			if(endereco_do_carro == -1) {
+				throw new PlacaNaoCadastradaException();
+			}
+			return endereco_do_carro;
+		} catch (PlacaNaoCadastradaException e) {
+			JOptionPane.showMessageDialog(null, e);
+			return -1;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 
-		ArrayList<Veiculo> veiculos = new ArrayList<>(); // criando uma arraylist do tipo veiculo
 		int opcaoMenu = -1;
 
 		do {
@@ -62,6 +84,10 @@ public class Main {
 
 					break;
 				case 2: // REGISTRAR DESPESAS
+					int i = EnderecoDoCarro();
+					if(i == -1) /* Para certeza de que o endereço não vai passar caso não encontre a placa do carro*/
+						break;
+					JOptionPane.showMessageDialog(null, "O veiculo de placa " + veiculos.get(i).getPlaca() + "cadastrado");
 					int opcaoDespesa = -1;
 					do {
 						try {
@@ -73,8 +99,8 @@ public class Main {
 							case 1: //REGISTRAR ABASTECIMENTO
 								
 								try {
-									veiculos.get(veiculos.size() - 1)
-											.addAbs(RegistrarAbastecimento.init(veiculos.get(veiculos.size() - 1))); //adicionar uma despesa no arraylist de despesas
+									veiculos.get(i)
+											.addAbs(RegistrarAbastecimento.init(veiculos.get(i))); //adicionar uma despesa no arraylist de despesas
 
 									//JOptionPane.showMessageDialog(null, veiculos.get(veiculos.size() - 1).toString()); //testando a leitura de despesas
 								} catch (Exception e) {
@@ -85,8 +111,8 @@ public class Main {
 							case 2: //REGISTRAR MANUTENCAO
 		
 								try {
-									veiculos.get(veiculos.size() - 1)
-											.addMA(RegistrarManutencao.init(veiculos.get(veiculos.size() - 1))); //adiciona uma manutencao no arraylist de manutencao
+									veiculos.get(i)
+											.addMA(RegistrarManutencao.init(veiculos.get(i))); //adiciona uma manutencao no arraylist de manutencao
 
 									//JOptionPane.showMessageDialog(null, veiculos.get(veiculos.size() - 1).toString()); //testando leitura de manutencao
 								} catch (Exception e) {
@@ -96,7 +122,7 @@ public class Main {
 
 							case 3: //REGISTAR TAXA(IMPOSTO, SEGURO, ETC)
 
-								veiculos.get(veiculos.size() - 1).addtax(RegistrarTaxa.init());  //adicionar taxa no arraylist de taxa
+								veiculos.get(i).addtax(RegistrarTaxa.init());  //adicionar taxa no arraylist de taxa
 								//JOptionPane.showMessageDialog(null, veiculos.get(veiculos.size() - 1).toString()); //testando leitura de taxa
 
 								break;
@@ -119,6 +145,9 @@ public class Main {
 					break;
 
 				case 3: // MOSTRAR RELATORIOS
+					i = EnderecoDoCarro();
+					if(i == -1) /* Para certeza de que o endereço não vai passar caso não encontre a placa do carro*/
+						break;
 					int opcaoRelatorio = -1;
 					do {
 						try {
@@ -130,21 +159,22 @@ public class Main {
 
 							case 1: // gerar relatorio simples de abastecimento
 								JOptionPane.showMessageDialog(null, RelatorioSimples
-										.GerarRelatorioAbastecimento(veiculos.get(veiculos.size() - 1).getAbs()));
+										.GerarRelatorioAbastecimento(veiculos.get(i).getAbs()));
 								break;
 
 							case 2: // gerar relatorio simples de manutencao
 								JOptionPane.showMessageDialog(null, RelatorioSimples
-										.GerarRelatorioManutencao(veiculos.get(veiculos.size() - 1).getMA()));
+										.GerarRelatorioManutencao(veiculos.get(i).getMA()));
 								break;
 
 							case 3: // gerar relatorio simples de taxa
 								JOptionPane.showMessageDialog(null, RelatorioSimples
-										.GerarRelatorioTaxa(veiculos.get(veiculos.size() - 1).getTAX()));
+										.GerarRelatorioTaxa(veiculos.get(i).getTAX()));
 								break;
 
 							case 4: // gerar relatorio de desempenho: consumo do veiculo
-
+								 JOptionPane.showMessageDialog(null, RelatorioConsumoDoVeiculo
+										.GerarRelatorioConsumoDoVeiculo(veiculos.get(i).getAbs()));
 								break;
 
 							case 5: // gerar relatorio de desempenho: custo do km rodado
